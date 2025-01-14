eb216b1f1ce261a5dece643639559ae84d9150b1
nils-developer <assel.nils.developer@web.de>
2025-01-14 19:29:29 +0100

    add contact formular

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template("index.html")
 
-def bye():
-    print("Bye!")
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    message = request.form.get("message")
 
+    print(f"Name: {name} Email: {email}, Message: {message}")
+
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
